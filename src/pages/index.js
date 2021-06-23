6fd4cc761ba6bb2687b9830234ae3837a7e28cb5
Ethan Rickaby <ejrickaby@gmail.com>
2021-06-23 20:22:57 +0930

    Added gatsby project

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>Welcome!</h1>
+    <p>My name is Ethan Rickaby and this is my personal website.</p>
+    <p>Let's build something great.</p>
+    {/* <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    /> */}
+  </Layout>
+)
+
+export default IndexPage
